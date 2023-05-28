a3d1063438f25e4288345a469a6e181c804268ac
Maciej Adamski <madamskip1@gmail.com>
2023-05-29 01:13:16 +0200

    TerrainLoader now load neighbours tiles of geo grids

--- app/src/main/java/com/example/peaksrecognition/terrain/TerrainLoader.java
+++ app/src/main/java/com/example/peaksrecognition/terrain/TerrainLoader.java
@@ -2,46 +2,43 @@ package com.example.peaksrecognition.terrain;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.util.Log;
+
+import com.example.peaksrecognition.CoordsManager;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.Vector;
 
 public class TerrainLoader {
-    private final String[][] filesNamesGrid = new String[][]
-            {
-                    { null, null, null },
-                    { "N49E019.hgt", "N49E020.hgt", null },
-                    { "N48E019.hgt", "N48E020.hgt", null }
-            };
-    private final float[] world_size = new float[] { 222.38985328911747f, 145.87864979682834f };
-    private final float[] grid_size = new float[] { 111.19492664455873f, 72.93932489841417f };
-    private final int[] newHgtSize = new int[] { 2402, 2402 };
-    private final float max_distance = 50.0f;
+    private final int[] newHgtSize = new int[]{2402, 2402};
+    private final double max_distance = 50.0;
     private final int observer_vertex_x = 793;
     private final int observer_vertex_z = 1298;
 
-    private final float LATITUDE_APPROXIMATION = 111.0f;
-    private final float LONGITUDE_APPROXIMATION = 73.0f;
-
-    private final float[] worldSize;
+    private final double[] worldSize;
     private final int[] hgtSize = new int[]{1201, 1201};
     private final Context context;
-    private float[] scale;
+    private double[] scale;
 
     private float[] vertices;
 
-
-
     public TerrainLoader(Context context) {
         this.context = context;
-        worldSize = new float[]{LATITUDE_APPROXIMATION, LONGITUDE_APPROXIMATION};
+        double observerLatitude = 49.339045;
+        double observerLongitude = 20.081936;
+        Vector<int[]> coords = prepareCoords(observerLatitude, observerLongitude, max_distance);
+        int[][] coordsRange = getCoordsRange(coords);
+        double[][] worldGridSize = calcWorldSize(coordsRange);
+        worldSize = worldGridSize[0];
+        String[][] filesNamesGrid = prepareFilesNamesGrid(coords, (int) observerLatitude, (int) observerLongitude);
         calcScale();
-        short[][] heightMap = loadHgtGrid();
+        short[][] heightMap = loadHgtGrid(filesNamesGrid);
         heightMap = dropUnusedData(heightMap);
+        generateVertices(heightMap);
+        //generateTriangles();
     }
 
     private short[][] loadHgtFile(String path) {
@@ -51,37 +48,28 @@ public class TerrainLoader {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        short[][] heightMap = convertHgtByteToArray(data);
-        return heightMap;
+        return convertHgtByteToArray(data);
     }
 
-    private short[][] loadHgtGrid()
-    {
+    private short[][] loadHgtGrid(String[][] filesNamesGrid) {
         int startRow = -1;
         int startCol = -1;
         int endRow = -1;
         int endCol = -1;
 
-        for (int i = 0; i < 3; ++i)
-        {
-            for (int j = 0; j < 3; ++j)
-            {
-                if (filesNamesGrid[i][j] != null)
-                {
-                    if (startRow == -1)
-                    {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (filesNamesGrid[i][j] != null) {
+                    if (startRow == -1) {
                         startRow = i;
                     }
-                    if (startCol == -1)
-                    {
+                    if (startCol == -1) {
                         startCol = j;
                     }
-                    if (endRow < i)
-                    {
+                    if (endRow < i) {
                         endRow = i;
                     }
-                    if (endCol < j)
-                    {
+                    if (endCol < j) {
                         endCol = j;
                     }
                 }
@@ -92,19 +80,15 @@ public class TerrainLoader {
         int cols = endCol - startCol + 1;
 
         short[][] elevation = new short[rows * 1201][cols * 1201];
-        for (int i = 0; i < rows; ++i)
-        {
-            for (int j = 0; j < cols; ++j)
-            {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
                 short[][] fileData = loadHgtFile("srtm_data/" + filesNamesGrid[i + startRow][j + startCol]);
                 int xOffset = i * 1201;
                 int zOffset = j * 1201;
 
-                for (int x = 0; x < 1201; ++x)
-                {
+                for (int x = 0; x < 1201; ++x) {
                     int rowIndex = x + xOffset;
-                    for (int z = 0; z < 1201; ++z)
-                    {
+                    for (int z = 0; z < 1201; ++z) {
                         int colIndex = z + zOffset;
                         short val = fileData[x][z];
                         elevation[rowIndex][colIndex] = val;
@@ -117,8 +101,7 @@ public class TerrainLoader {
         return elevation;
     }
 
-    private short[][] dropUnusedData(short[][] heightMap)
-    {
+    private short[][] dropUnusedData(short[][] heightMap) {
         int range_x = (int) Math.ceil(max_distance / scale[0]);
         int range_z = (int) Math.ceil(max_distance / scale[2]);
 
@@ -131,14 +114,13 @@ public class TerrainLoader {
         x_end = Math.min(x_end, newHgtSize[0]);
         z_end = Math.min(z_end, newHgtSize[1]);
 
-        float origin_x = 0.0f + (float)x_start * scale[0];
-        float origin_z = 0.0f + (float)z_start * scale[2];
+        double origin_x = 0.0f + x_start * scale[0];
+        double origin_z = 0.0f + z_start * scale[2];
 
         short[][] newHeightMap = new short[x_end - x_start + 1][z_end - z_start + 1];
         for (int i = x_start; i <= x_end; i++) {
-            for (int j = z_start; j <= z_end; j++) {
-                newHeightMap[i - x_start][j - z_start] = heightMap[i][j];
-            }
+            if (z_end + 1 - z_start >= 0)
+                System.arraycopy(heightMap[i], z_start, newHeightMap[i - x_start], 0, z_end + 1 - z_start);
         }
         heightMap = null;
         return newHeightMap;
@@ -162,8 +144,8 @@ public class TerrainLoader {
         short[][] heightMap = new short[numRows][numColumns];
         ByteBuffer byteBuffer = ByteBuffer.wrap(data);
         byteBuffer.order(ByteOrder.BIG_ENDIAN);  // The byte order may vary, adjust accordingly
-        int arrayRow = 0;
-        int arrayCol = 0;
+        int arrayRow;
+        int arrayCol;
 
         for (int i = 0; i < initHgtSize; ++i) {
             arrayCol = 0;
@@ -185,23 +167,24 @@ public class TerrainLoader {
     }
 
     private void calcScale() {
-        float xScale = worldSize[0] / (float) (hgtSize[0] - 1);
-        float yScale = (float) (1.0 / 1000.0);
-        float zScale = worldSize[1] / (float) (hgtSize[1] - 1);
+        double xScale = worldSize[0] / (hgtSize[0] - 1);
+        double yScale = (1.0 / 1000.0);
+        double zScale = worldSize[1] / (hgtSize[1] - 1);
 
-        scale = new float[]{xScale, yScale, zScale};
+        scale = new double[]{xScale, yScale, zScale};
     }
 
     private void generateVertices(short[][] heightMap) {
+        // TODO: Fix after adding dropUnusedData
         float[] vertices = new float[1201 * 1201 * 3];
         int verticesIndex = 0;
 
         for (int x = 0; x < 1201; ++x) {
             for (int z = 0; z < 1201; ++z) {
                 short y = heightMap[x][z];
-                float xCoord = 0.0f + scale[0] * x;
-                float yCoord = 0.0f + scale[1] * y;
-                float zCoord = 0.0f + scale[2] * z;
+                float xCoord = (float) (0.0 + scale[0] * x);
+                float yCoord = (float) (0.0 + scale[1] * y);
+                float zCoord = (float) (0.0 + scale[2] * z);
                 vertices[verticesIndex] = xCoord;
                 vertices[verticesIndex + 1] = yCoord;
                 vertices[verticesIndex + 2] = zCoord;
@@ -212,7 +195,7 @@ public class TerrainLoader {
     }
 
     private void generateTriangles() {
-        int trianglesNum = ((1201 - 1) * (1201 - 1) *2);
+        int trianglesNum = ((1201 - 1) * (1201 - 1) * 2);
         int[] triangles = new int[trianglesNum * 3];
 
         int trianglesIndex = 0;
@@ -240,6 +223,163 @@ public class TerrainLoader {
             }
             ++index;
         }
+    }
+
+    private String convertCoordsToFileName(int latitude, int longitude) {
+        String latitudeStr = String.valueOf(Math.abs(latitude));
+        String longitudeStr = String.valueOf(Math.abs(longitude));
+
+        if (latitude > -10 && latitude < 10) {
+            latitudeStr = "0" + latitudeStr;
+        }
+
+        if (longitude > -100 && longitude < 100) {
+            if (longitude > -10 && longitude < 10) {
+                longitudeStr = "00" + longitudeStr;
+            } else {
+                longitudeStr = "0" + longitudeStr;
+            }
+        }
+        String fileName;
+        if (latitude >= 0) {
+            fileName = "N" + latitudeStr;
+        } else {
+            fileName = "S" + latitudeStr;
+        }
+
+        if (longitude >= 0) {
+            fileName += "E" + longitudeStr;
+        } else {
+            fileName += "W" + longitudeStr;
+        }
+
+        return fileName;
+    }
+
+    private Vector<int[]> prepareCoords(double observerLatitude, double observerLongitude, double maxDistance) {
+        int latitudeInt = (int) observerLatitude;
+        int longitudeInt = (int) observerLongitude;
+        Vector<double[]> coordsToCheck = new Vector<>();
+        Vector<int[]> coordsForFilesNames = new Vector<>();
+        double[] y;
+        double[] x;
+
+        if (observerLatitude > 0.0) {
+            y = new double[]{latitudeInt, observerLatitude, latitudeInt + 1.0};
+        } else {
+            y = new double[]{latitudeInt - 1, observerLatitude, latitudeInt};
+        }
+
+        if (observerLongitude > 0.0) {
+            x = new double[]{longitudeInt, observerLongitude, longitudeInt + 1.0};
+        } else {
+            x = new double[]{longitudeInt - 1, observerLongitude, longitudeInt};
+        }
+
+        if (observerLatitude == latitudeInt && observerLongitude == longitudeInt) {
+            for (int i = 0; i < 2; ++i) {
+                for (int j = 0; j < 2; ++j) {
+                    coordsForFilesNames.add(new int[]{latitudeInt - i, longitudeInt - j});
+                }
+            }
+        } else if (observerLatitude == latitudeInt) {
+            coordsForFilesNames.add(new int[]{latitudeInt - 1, (int) x[0]});
+            coordsForFilesNames.add(new int[]{latitudeInt, (int) x[0]});
+
+            for (int i = 0; i < 2; ++i) {
+                coordsToCheck.add(new double[]{latitudeInt, x[0], latitudeInt - i, x[0] - 1});
+                coordsToCheck.add(new double[]{latitudeInt, x[2], latitudeInt - i, x[0] + 1});
+            }
+        } else if (observerLongitude == longitudeInt) {
+            coordsForFilesNames.add(new int[]{(int) y[0], longitudeInt - 1});
+            coordsForFilesNames.add(new int[]{(int) y[0], longitudeInt});
+
+            for (int i = 0; i < 2; ++i) {
+                coordsToCheck.add(new double[]{y[0], longitudeInt, y[0] - 1, longitudeInt - i});
+                coordsToCheck.add(new double[]{y[1], longitudeInt, y[0] + 1, longitudeInt - i});
+            }
+        } else {
+            coordsForFilesNames.add(new int[]{latitudeInt, longitudeInt});
+
+            for (int i = 0; i < 3; ++i) { // latitude
+
+                double yCoordToCheck = y[i];
+                int yCoordFile = (int) y[0] + (i - 1);
+
+                for (int j = 0; j < 3; ++j) { // longitude
+                    if (!(i == 1 && j == 1)) {
+                        double xCoordToCheck = x[j];
+                        int xCoordFile = (int) x[0] + (j - 1);
+                        coordsToCheck.add(new double[]{yCoordToCheck, xCoordToCheck, yCoordFile, xCoordFile});
+                    }
+                }
+            }
+        }
+
+        for (double[] coordsData : coordsToCheck) {
+            double distance = CoordsManager.equirectangularApproximation(coordsData[0], coordsData[1],
+                    observerLatitude, observerLongitude);
+            if (distance <= maxDistance) {
+                coordsForFilesNames.add(new int[]{(int) coordsData[2], (int) coordsData[3]});
+            }
+        }
+
+        return coordsForFilesNames;
+    }
+
+    private int[][] getCoordsRange(Vector<int[]> coords) {
+        int minLatitude = 1000;
+        int maxLatitude = -1000;
+        int minLongitude = 1000;
+        int maxLongitude = -1000;
+
+        for (int[] coord : coords) {
+            minLatitude = Math.min(minLatitude, coord[0]);
+            maxLatitude = Math.max(maxLatitude, coord[0]);
+            minLongitude = Math.min(minLongitude, coord[1]);
+            maxLongitude = Math.max(maxLongitude, coord[1]);
+        }
+        ++maxLatitude;
+        ++maxLongitude;
+
+        return new int[][]{{minLatitude, maxLatitude}, {minLongitude, maxLongitude}};
+    }
+
+    private double[][] calcWorldSize(int[][] coordsRange) {
+        int[] latitudeRange = coordsRange[0];
+        int[] longitudeRange = coordsRange[1];
+        double latitudeDistance = 0.0;
+        double longitudeDistance = 0.0;
+
+        for (int i = 0; i < 2; ++i) {
+            latitudeDistance += CoordsManager.equirectangularApproximation(latitudeRange[0], longitudeRange[i],
+                    latitudeRange[1], longitudeRange[i]);
+            longitudeDistance += CoordsManager.equirectangularApproximation(latitudeRange[i], longitudeRange[0],
+                    latitudeRange[i], longitudeRange[1]);
+        }
+
+        latitudeDistance /= 2.0;
+        longitudeDistance /= 2.0;
+        double latitudeGridSize = latitudeDistance / (double) Math.abs(latitudeRange[1] - latitudeRange[0]);
+        double longitudeGridSize = longitudeDistance / (double) Math.abs(longitudeRange[1] - longitudeRange[0]);
+
+        return new double[][]{{latitudeDistance, longitudeDistance}, {latitudeGridSize, longitudeGridSize}};
+    }
+
+    private String[][] prepareFilesNamesGrid(Vector<int[]> coords, int centralLatitude, int centralLongitude) {
+        String[][] filesNamesGrid = new String[][]{
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+        };
+        for (int[] coord : coords) {
+            int latitudeDiff = coord[0] - centralLatitude;
+            int longitudeDiff = coord[1] - centralLongitude;
+            filesNamesGrid[1 - latitudeDiff][1 + longitudeDiff] = convertCoordsToFileName(coord[0], coord[1]) + ".hgt";
+        }
+
+
+        return filesNamesGrid;
     }
 
 }
