9141d24a6c4e97ba2ac871933d4299dea380b8c1
Maciej Adamski <madamskip1@gmail.com>
2023-06-10 15:57:15 +0200

    Add Rotationmanager class

---
+++ app/src/main/java/com/example/peaksrecognition/RotationManager.java
@@ -0,0 +1,60 @@
+package com.example.locationrotationclass;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class RotationManager implements SensorEventListener {
+    private RotationListener rotationListener;
+    private final SensorManager sensorManager;
+    private final Sensor rotationSensor;
+
+    public RotationManager(Context context) {
+        ,,sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    }
+
+    public void addRotationListener(RotationListener rotationListener)
+    {
+        this.rotationListener = rotationListener;
+        sensorManager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float[] rotationVector = calcRotationVector(sensorEvent);
+        rotationListener.onRotationChanged(rotationVector);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public interface RotationListener
+    {
+        void onRotationChanged(float[] rotationVector);
+    }
+
+    private float[] calcRotationVector(SensorEvent sensorEvent)
+    {
+        float[] rotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, sensorEvent.values);
+
+        float[] adjustedRotationMatrix = new float[9];
+        SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z,
+                adjustedRotationMatrix);
+
+        float[] orientation = new float[3];
+        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
+
+        for(int i = 0; i < 3; i++) {
+            orientation[i] = (float)(Math.toDegrees(orientation[i]));
+        }
+
+        return orientation;
+    }
+}
