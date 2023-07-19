56dedf0bfffd4a07d70121f5113e1b1aadf2b40e
Maciej Adamski <madamskip1@gmail.com>
2023-07-20 00:48:57 +0200

    CannyThresholdsLive now use CameraX instead of opencv's camera'

--- app/src/main/java/com/example/peaksrecognition/activities/CannyThresholdLiveActivity.java
+++ app/src/main/java/com/example/peaksrecognition/activities/CannyThresholdLiveActivity.java
@@ -1,8 +1,9 @@
 package com.example.peaksrecognition.activities;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,21 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.example.peaksrecognition.Camera;
-import com.example.peaksrecognition.CoordsManager;
-import com.example.peaksrecognition.LocationManager;
+import com.example.peaksrecognition.CannyThresholdLive;
 import com.example.peaksrecognition.R;
-import com.example.peaksrecognition.RotationManager;
-import com.example.peaksrecognition.mainopengl.OffScreenRenderer;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
-public class CannyThresholdLiveActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class CannyThresholdLiveActivity extends AppCompatActivity {
     private static final int MY_CAMERA_REQUEST_CODE = 100;
 
     static {
@@ -38,98 +30,29 @@ public class CannyThresholdLiveActivity extends AppCompatActivity implements Cam
     SeekBar highBar;
     int lowThreshold = 0;
     int highThreshold = 0;
-    JavaCameraView javaCameraView;
-    int activeCamera = CameraBridgeViewBase.CAMERA_ID_BACK;
-
-
-    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            if (status == BaseLoaderCallback.SUCCESS) {
-                javaCameraView.enableView();
-            } else {
-                super.onManagerConnected(status);
-            }
-        }
-    };
+    CannyThresholdLive cannyThresholdLive;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == MY_CAMERA_REQUEST_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initializeCamera(javaCameraView, activeCamera);
+
             }
         }
     }
 
-    private void initializeCamera(JavaCameraView javaCameraView, int activeCamera) {
-        javaCameraView.setCameraPermissionGranted();
-        javaCameraView.setCameraIndex(activeCamera);
-        javaCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
-        javaCameraView.setCvCameraViewListener(this);
-    }
-
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat gray = inputFrame.gray();
-        int height = gray.height();
-        int width = gray.width();
-
-        Mat edge = new Mat();
-        Imgproc.Canny(gray, edge, lowThreshold, highThreshold);
-        gray.release();
-        return edge; // This function must return
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if (javaCameraView != null) {
-            javaCameraView.disableView();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (javaCameraView != null) {
-            javaCameraView.disableView();
-        }
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (OpenCVLoader.initDebug()) {
-            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
-        } else {
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        } else {
+            // Camera permission is already granted
+        }
         setContentView(R.layout.activity_canny_threshold_live);
         lowTextView = findViewById(R.id.liveThresholdLowText);
         highTextView = findViewById(R.id.liveThresholdHighText);
@@ -144,6 +67,7 @@ public class CannyThresholdLiveActivity extends AppCompatActivity implements Cam
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 lowTextView.setText(String.valueOf(i));
                 lowThreshold = i;
+                cannyThresholdLive.setThreshold(lowThreshold, highThreshold);
             }
 
             @Override
@@ -161,6 +85,7 @@ public class CannyThresholdLiveActivity extends AppCompatActivity implements Cam
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 highTextView.setText(String.valueOf(i));
                 highThreshold = i;
+                cannyThresholdLive.setThreshold(lowThreshold, highThreshold);
             }
 
             @Override
@@ -174,14 +99,17 @@ public class CannyThresholdLiveActivity extends AppCompatActivity implements Cam
             }
         });
 
-        javaCameraView = findViewById(R.id.java_camera_view);
 
         // checking if the permission has already been granted
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_GRANTED) {
-            initializeCamera(javaCameraView, activeCamera);
         } else {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
         }
+
+        cannyThresholdLive = new CannyThresholdLive(this, findViewById(R.id.cannyLiveTestImageView), 640, 480);
+        cannyThresholdLive.setThreshold(lowThreshold, highThreshold);
+        cannyThresholdLive.start();
     }
+
 }
