ca6f8926a6088e04e9cb9b387615a485eadd837d
Maciej Adamski <madamskip1@gmail.com>
2023-09-11 02:03:42 +0200

    Add Haversian method calculating geo distance

---
+++ projekt/app/src/main/java/org/pw/masterthesis/peaksrecognition/geodistance/Haversian.java
@@ -0,0 +1,21 @@
+package org.pw.masterthesis.peaksrecognition.geodistance;
+
+public class Haversian implements GeoDistance {
+    @Override
+    public double calcDistance(double latitude1, double longitude1, double latitude2, double longitude2) {
+        double deltaLatitude = Math.toRadians(latitude2 - latitude1);
+        double deltaLongitude = Math.toRadians(longitude2 - longitude1);
+
+        double latitude1Radians = Math.toRadians(latitude1);
+        double latitude2Radians = Math.toRadians(latitude2);
+
+        double sinDeltaLatitude = Math.sin(deltaLatitude / 2.0);
+        double sinDeltaLongitude = Math.sin(deltaLongitude / 2.0);
+
+        double a = sinDeltaLatitude * sinDeltaLatitude + Math.cos(latitude1Radians) * Math.cos(latitude2Radians) * sinDeltaLatitude * sinDeltaLongitude;
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = EARTH_RADIUS * c;
+
+        return distance;
+    }
+}
